0e71398b7957298a5cf5ce6ecce961eda29f042a
Natalia <nataliamaria868@gmail.com>
2023-11-03 18:49:12 -0300

    Commit acrescentando JS

---
+++ script.js
@@ -0,0 +1,8 @@
+
+const text2 = document.getElementById('.parte2');
+const btn = document.querySelector('.botao');
+
+btn.addEventListener('click', function() {
+    text2.innerHTML = button.botao
+})
+
